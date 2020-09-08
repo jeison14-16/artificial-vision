3226c0d010ebb300105e1fb0e3438f5cefabd203
jeison14-16 <jeison14-16@hotmail.com>
2020-09-08 12:04:12 -0500

    Basic Calcs v1, v2, v3

---
+++ basic_cal2.py
@@ -0,0 +1,14 @@
+#Script: this program let you add two numbers entered by the user.
+
+#clear screen class
+import os
+
+os.system("cls")
+
+print("Enter first number: ")
+x = int(input())
+
+y = int(input("Enter second number: ")) 
+
+add = x + y
+print("The add is: ", add)
---
+++ myAge.py
@@ -0,0 +1,25 @@
+import os
+
+os.system("cls")
+'''
+What does this script do?:
+This script show your name and your age.
+'''
+
+'''
+How config git in my computer?
+1st. git config --global user.name "myusername"
+2nd. git config --global user.email "mygitemail"
+How make a tracking/staging to GitHub or GitLab
+1st. git add . / git add --all / git add file_name
+2nd. git commit -m "Your message"
+3rd. git push origin master 
+''''
+
+myName = 'Jaisson. Criollo'
+myDate = 1994
+year = 2020
+
+age = year - myDate
+
+print ("Your name is: ", myName, "and you are ", age, "years old")
